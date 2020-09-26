32e38c3670a8e69bb55543f0b9ff0442e7bf12bc
Your Namestefen <you@example.com394972404@qq.com>
2020-09-26 08:59:27 +0800

    项目完成

--- BigNews/admin/libs/internter.js
+++ BigNews/admin/libs/internter.js
@@ -19,6 +19,19 @@
         comment_pass: baseUrl + '/admin/comment/pass',//文章评论通过
         comment_reject: baseUrl + '/admin/comment/reject',//文章评论不通过
         comment_delete: baseUrl + '/admin/comment/delete',//文章评论删除
+        index_hotpic:baseUrl+"/index/hotpic",//首页焦点图
+        index_category: baseUrl+"/index/category",//文章类型
+        index_latest:baseUrl+"/index/latest",//最新资讯
+        index_rank:baseUrl+"/index/rank",//热门排行
+        index_comment:baseUrl+"/index/latest_comment",//最新评论
+        index_attention:baseUrl+"/index/attention",//焦点关注
+        index_artitle:baseUrl+"/index/article",//文章详细内容
+        index_get_comment:baseUrl+"/index/get_comment",//评论列表
+        index_post_comment:baseUrl+"/index/post_comment",//发表评论
+        index_search:baseUrl+"/index/search",//文章搜索
+        countArticle: baseUrl+"/admin/data/article", //文章日新增统计
+        countIfon:baseUrl+"/admin/data/info", //获取统计数据
+        countCate:baseUrl+"/admin/data/category",//获取统计类别
     };
     w.bigNews = bigNews;
 })(window)
